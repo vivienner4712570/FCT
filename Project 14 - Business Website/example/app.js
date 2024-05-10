1d62061359417d19ccdd8f4e615d9073f679f605
abigailsteitz <abigailsteitz@gmail.com>
2024-05-10 12:13:34 -0700

    Project 14 - Business Website initial commit. Made dog and cat picture local files.

---
+++ Project 14 - Business Website/example/app.js
@@ -0,0 +1,20 @@
+// Switch to strict mode to get more useful errors when you make mistakes.
+'use strict';
+
+function secretMessage() {
+    alert("You have unlocked the secret message!")
+}
+function displayDog() {
+    document.getElementById("dogContent").style.display = "block";
+    document.getElementById("catContent").style.display = "none";
+    document.getElementById("defaultContent").style.display = "none";
+}
+function displayCat() {
+    document.getElementById("dogContent").style.display = "none";
+    document.getElementById("catContent").style.display = "block";
+    document.getElementById("defaultContent").style.display = "none";
+}
+function redBackground() {
+    document.body.style.backgroundColor = "red";
+    console.log(document)
+}
