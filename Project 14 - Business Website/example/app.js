fbcbb662560cc379569ca3a6078e4eb3b8e51529
tylerhuntseattleacademy <tylerhunt@seattleacademy.org>
2024-05-22 09:04:14 -0700

    video game quiz
    quiz

--- Project 14 - Business Website/example/app.js
+++ Project 14 - Business Website/example/app.js
@@ -2,19 +2,27 @@
 'use strict';
 
 function secretMessage() {
-    alert("You have unlocked the secret message!")
+    alert("Skibidi toilet!")
 }
 function displayDog() {
-    document.getElementById("dogContent").style.display = "block";
-    document.getElementById("catContent").style.display = "none";
+    document.getElementById("MCContent").style.display = "block";
+    document.getElementById("FNContent").style.display = "none";
+    document.getElementById("SUSContent").style.display = "none";
     document.getElementById("defaultContent").style.display = "none";
 }
 function displayCat() {
-    document.getElementById("dogContent").style.display = "none";
-    document.getElementById("catContent").style.display = "block";
+    document.getElementById("MCContent").style.display = "none";
+    document.getElementById("FNContent").style.display = "block";
+    document.getElementById("SUSContent").style.display = "none";
     document.getElementById("defaultContent").style.display = "none";
 }
-function redBackground() {
-    document.body.style.backgroundColor = "red";
+function greenBackground() {
+    document.body.style.backgroundColor = "green";
     console.log(document)
+}
+function displaySUS() {
+    document.getElementById("MCContent").style.display = "none";
+    document.getElementById("FNContent").style.display = "none";
+    document.getElementById("SUSContent").style.display = "block";
+    document.getElementById("defaultContent").style.display = "none";
 }
