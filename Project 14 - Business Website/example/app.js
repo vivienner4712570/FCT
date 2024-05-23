1e86e1a07d1fc1124fa378755fcd9ee93012da99
saframarie <safracrawford@seattleacademy.org>
2024-05-23 14:30:50 -0700

    bend website

--- Project 14 - Business Website/example/app.js
+++ Project 14 - Business Website/example/app.js
@@ -2,19 +2,19 @@
 'use strict';
 
 function secretMessage() {
-    alert("You have unlocked the secret message!")
+    alert("You have unlocked the 10% discount!")
 }
-function displayDog() {
-    document.getElementById("dogContent").style.display = "block";
-    document.getElementById("catContent").style.display = "none";
+function displayShirt() {
+    document.getElementById("shirtContent").style.display = "block";
+    document.getElementById("sweatshirtContent").style.display = "none";
     document.getElementById("defaultContent").style.display = "none";
 }
-function displayCat() {
-    document.getElementById("dogContent").style.display = "none";
-    document.getElementById("catContent").style.display = "block";
+function displaySweatshirt() {
+    document.getElementById("shirtContent").style.display = "none";
+    document.getElementById("sweatshirtContent").style.display = "block";
     document.getElementById("defaultContent").style.display = "none";
 }
-function redBackground() {
-    document.body.style.backgroundColor = "red";
+function blueBackground() {
+    document.body.style.backgroundColor = "blue";
     console.log(document)
 }
